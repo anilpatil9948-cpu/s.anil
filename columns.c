471d106f3db388461573d50714d3a7af86fd8f91
anilpatil9948-cpu <anilpatil9948@gmail.com>
2026-02-04 11:08:28 +0530

    Add files via upload

---
+++ columns.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+int column;
+printf("enter the no of columns");
+scanf("%d",&column);
+for(int i=1; i<=column;i++)
+printf(" *");
+}
